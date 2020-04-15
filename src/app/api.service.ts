b9d4ddc2cd6d5788fa089a58036d77bca3f7a685
Kyra Buchmann <kac580@outlook.com>
2020-04-14 22:11:52 -0500

    update api date

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -12,7 +12,7 @@ export class ApiService {
     ) { }
   getHistorical(location){
     return this.http.get(
-      "https://api.weatherstack.com/historical?access_key=7d943ce723d7ddea226fd1806a40fb60&historical_date=2020-04-12&units=f&hourly=1&interval=3&query=" +
+      "https://api.weatherstack.com/historical?access_key=7d943ce723d7ddea226fd1806a40fb60&historical_date=2020-04-14&units=f&hourly=1&interval=3&query=" +
       location 
     )
   }
